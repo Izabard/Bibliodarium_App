73c81263a361d6f0f3e955d1ad1cfe440b99f61f
Izabard <izaba97@gmail.com>
2019-02-20 22:35:40 -0600

    Credentials are private

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,8 +1,8 @@
 
 if Rails.env.production?
     CarrierWave.configure do |config|
-        config.azure_storage_account_name =  'izaba',
-        config.azure_storage_access_key = 'inOgkKlKEuW/vkOqMxhDJ8Or7OLe13RO7f9vtCIKj6Fhdy9hwHNc8MIkPHseVf9xxO8OSD07vm4ba2zDsnE8sA==',
-        config.azure_container = 'bibliodarium-app-covers'
+        config.azure_storage_account_name =  ENV['STORAGE_ACCOUNT_NAME'],
+        config.azure_storage_access_key = ENV['STORAGE_SECRET_KEY'],
+        config.azure_container = ENV['STORAGE_CONTAINER']
     end
 end
