b761c560d402f12781ae8517ad24616ea5cf16de
Izabard <izaba@gmail.com>
2019-02-22 21:13:24 -0600

    Azure connection 9

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,16 +2,16 @@
 if Rails.env.production?
     require 'azure/storage/blob'
 
-    account_name = 'izaba'   
-    account_key = 'inOgkKlKEuW/vkOqMxhDJ8Or7OLe13RO7f9vtCIKj6Fhdy9hwHNc8MIkPHseVf9xxO8OSD07vm4ba2zDsnE8sA=='
+    #account_name = 'izaba'   
+    #account_key = 'inOgkKlKEuW/vkOqMxhDJ8Or7OLe13RO7f9vtCIKj6Fhdy9hwHNc8MIkPHseVf9xxO8OSD07vm4ba2zDsnE8sA=='
     container_name = 'bibliodarium_app_covers'
     #store_dir = "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     blob_client = Azure::Blob::BlobService
 
     CarrierWave.configure do |config|
         blob_client = Azure::Storage::Blob::BlobService.create(
-            storage_account_name: account_name,
-            storage_access_key: account_key
+            storage_account_name: 'izaba',
+            storage_access_key: 'inOgkKlKEuW/vkOqMxhDJ8Or7OLe13RO7f9vtCIKj6Fhdy9hwHNc8MIkPHseVf9xxO8OSD07vm4ba2zDsnE8sA=='
         )
     end
     #blob_client.create_block_blob(container_name, store_dir, store_dir)
