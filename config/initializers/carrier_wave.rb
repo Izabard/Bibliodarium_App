bcd44ab5e87f0333f3f7ae36f1a5c8b061f17b21
Izabard <izaba@gmail.com>
2019-02-22 20:45:12 -0600

    Azure connection 1

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,8 +1,12 @@
 
 if Rails.env.production?
     require 'azure/storage/blob'
+
     account_name = 'izaba'   
     account_key = 'inOgkKlKEuW/vkOqMxhDJ8Or7OLe13RO7f9vtCIKj6Fhdy9hwHNc8MIkPHseVf9xxO8OSD07vm4ba2zDsnE8sA=='
+    container_name = 'bibliodarium_app_covers'
+
+    blob_client = Azure::Storage::Blob::BlobService
 
     CarrierWave.configure do |config|
         blob_client = Azure::Storage::Blob::BlobService.create(
@@ -10,4 +14,5 @@ if Rails.env.production?
             storage_access_key: account_key
         )
     end
+    config.directory = blob_client.create_block_blob(container.name, :storage)
 end
