febe640d2f163e05d2760d63b9fea22bf150093c
Izabard <izaba@gmail.com>
2019-02-22 21:53:58 -0600

    Azure connection 13

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,16 +6,16 @@ if Rails.env.production?
     #account_key = 'inOgkKlKEuW/vkOqMxhDJ8Or7OLe13RO7f9vtCIKj6Fhdy9hwHNc8MIkPHseVf9xxO8OSD07vm4ba2zDsnE8sA=='
     container_name = 'bibliodarium_app_covers'
     #store_dir = "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    blob_client = Azure::Blob::BlobService
-
-    signer = Azure::Core::Auth::SharedAccessSignatureSigner.new izaba, inOgkKlKEuW/vkOqMxhDJ8Or7OLe13RO7f9vtCIKj6Fhdy9hwHNc8MIkPHseVf9xxO8OSD07vm4ba2zDsnE8sA==
-    blob_client = Azure::Blob::BlobService.new(signer: signer)
+    blob_client = Azure::Blob::BlobService.
         
     CarrierWave.configure do |config|
-        blob_client = Azure::Storage::Blob::BlobService.create(
-            storage_account_name: ENV['STORAGE_ACCOUNT_NAME'],
-            storage_access_key: ENV['STORAGE_ACCESS_KEY']
-        )
+        azure_credentials = {
+          access_key_id: "izaba",
+          secret_access_key: 'inOgkKlKEuW/vkOqMxhDJ8Or7OLe13RO7f9vtCIKj6Fhdy9hwHNc8MIkPHseVf9xxO8OSD07vm4ba2zDsnE8sA=='
+        }
+        container = "bibliodarium_app_covers"
+
+        
 
         #blob_properties = blob_client.get_blob_properties container_name, blob_name
 
