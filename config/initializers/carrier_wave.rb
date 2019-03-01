b7c72dde82243ae4eee9c73689e241ea43333ef1
Izabard <izaba@gmail.com>
2019-03-01 16:24:56 -0600

    carrierwave changed

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,23 +2,10 @@
 if Rails.env.production?
     require 'azure/storage/blob'
 
-    #account_name = 'izaba'   
-    #account_key = 'inOgkKlKEuW/vkOqMxhDJ8Or7OLe13RO7f9vtCIKj6Fhdy9hwHNc8MIkPHseVf9xxO8OSD07vm4ba2zDsnE8sA=='
-    container_name = 'bibliodarium_app_covers'
-    #store_dir = "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    blob_client = Azure::Blob::BlobService
+    client = Azure::Storage::Client.create(:storage_account_name => 'izaba', :storage_access_key => 'inOgkKlKEuW/vkOqMxhDJ8Or7OLe13RO7f9vtCIKj6Fhdy9hwHNc8MIkPHseVf9xxO8OSD07vm4ba2zDsnE8sA==')
         
     CarrierWave.configure do |config|
-        azure_credentials = {
-          account_name: "izaba",
-          account_key: 'inOgkKlKEuW/vkOqMxhDJ8Or7OLe13RO7f9vtCIKj6Fhdy9hwHNc8MIkPHseVf9xxO8OSD07vm4ba2zDsnE8sA=='
-        }
-        container = "bibliodarium_app_covers"
-
-        
-
-        #blob_properties = blob_client.get_blob_properties container_name, blob_name
-
+        options = { :client => client }
+        blob_client = Azure::Storage::Blob::BlobService.create options
     end
-    #blob_client.create_block_blob(container_name, store_dir, store_dir)
 end
