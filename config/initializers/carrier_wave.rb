6bffacf769c43e2e139030a8bb0ac611db382652
Izabard <izaba@gmail.com>
2019-03-02 23:06:52 -0600

    credentials changed

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,7 +5,7 @@ if Rails.env.production?
     CarrierWave.configure do |config|
     	FogStorage = Fog::Storage::AzureRM.new(
     		azure_storage_account_name: "izaba",
-    		azure_storage_access_key: account_key,
+    		azure_storage_access_key: "inOgkKlKEuW/vkOqMxhDJ8Or7OLe13RO7f9vtCIKj6Fhdy9hwHNc8MIkPHseVf9xxO8OSD07vm4ba2zDsnE8sA==",
     		environment: '<AzureCloud>'
     	)
     	StorageBucket = FogStorage.directories.new key: CONFIG["bucket"]
