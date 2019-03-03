a6a8f8f4113beb4196ccb8344b6a8ae7e52a3a7c
Izabard <izaba@gmail.com>
2019-03-02 18:04:13 -0600

    modifying azure conection

--- Gemfile
+++ Gemfile
@@ -44,6 +44,7 @@ group :production do
   gem 'pg', '0.18.4'
   gem 'azure-storage-blob'
   gem 'carrierwave-azure'
+  gem 'fog-azure-rm'
 end
 
 
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,16 @@
 
 if Rails.env.production?
-    require 'azure/storage/blob'
+    require 'fog/azurerm'
 
-    client = Azure::Storage::Client.create(:storage_account_name => 'izaba', :storage_access_key => 'inOgkKlKEuW/vkOqMxhDJ8Or7OLe13RO7f9vtCIKj6Fhdy9hwHNc8MIkPHseVf9xxO8OSD07vm4ba2zDsnE8sA==')
+    storage_account_name = 'izaba', 
+    storage_access_key = 'inOgkKlKEuW/vkOqMxhDJ8Or7OLe13RO7f9vtCIKj6Fhdy9hwHNc8MIkPHseVf9xxO8OSD07vm4ba2zDsnE8sA=='
         
     CarrierWave.configure do |config|
-        options = { :client => client }
-        blob_client = Azure::Storage::Blob::BlobService.create options
+    	FogStorage = Fog::Storage::AzureRM.new(
+    		azure_storage_account_name: storage_account_name,
+    		azure_storage_access_key: storage_access_key,
+    		environment: '<AzureCloud>'
+    	)
+    	StorageBucket = FogStorage.directories.new key: CONFIG["bucket"]
     end
 end
