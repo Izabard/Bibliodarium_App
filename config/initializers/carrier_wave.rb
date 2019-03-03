ca4be623d42c71981c1211dfbe25b41839722c08
Izabard <izaba@gmail.com>
2019-03-02 23:11:45 -0600

    added new blob

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -8,6 +8,6 @@ if Rails.env.production?
     		azure_storage_access_key: "inOgkKlKEuW/vkOqMxhDJ8Or7OLe13RO7f9vtCIKj6Fhdy9hwHNc8MIkPHseVf9xxO8OSD07vm4ba2zDsnE8sA==",
     		environment: '<AzureCloud>'
     	)
-    	StorageBucket = FogStorage.directories.new key: CONFIG["bucket"]
+    	StorageBucket = FogStorage.directories.new key: "covers-book-blob"
     end
 end
